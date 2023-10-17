aa452567cd19cd576b62c009f85539556e613b45
XavierSpycy <115918068+XavierSpycy@users.noreply.github.com>
2023-10-18 00:22:32 +1100

    Add docstrings

--- eval/metrics.py
+++ eval/metrics.py
@@ -1,4 +1,14 @@
 import numpy as np
 
-def accuracy(y_true, y_pred):
+def accuracy(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    """
+    Calculate the accuracy.
+
+    Parameters:
+    y_true: np.ndarray. The true label.
+    y_pred: np.ndarray. The predicted label.
+
+    Returns:
+    float. The accuracy.
+    """
     return np.mean(y_true == y_pred)
