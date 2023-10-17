a364f27dc69e274f5d750762e7e2c959aba4f7cc
XavierSpycy <115918068+XavierSpycy@users.noreply.github.com>
2023-10-18 00:23:28 +1100

    Add docstrings

--- datasets/data_loader.py
+++ datasets/data_loader.py
@@ -1,11 +1,30 @@
+import os
 import numpy as np
+from typing import Tuple
 
-def datasets(file_path='./data/', train=True):
-    if train:
-        mode = 'train'
-    else:
-        mode = 'test'
+def datasets(file_path: str='./data/', train: bool=True) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Load the data and label.
+
+    Parameters:
+    file_path: str. The path of the data.
+    train: bool. Whether to load the train data or test data.
+
+    Returns:
+    X: np.ndarray. The data.
+    y: np.ndarray. The label.
+
+    Raises:
+    FileNotFoundError: If the file is not found.
+    """
+
+    mode = 'train' if train else 'test'
     
-    X = np.load(file_path + mode + '_data.npy')
-    y = np.load(file_path + mode + '_label.npy').reshape(-1)
+    try:
+        # Load the data and label using os.path.join for better path handling
+        X = np.load(os.path.join(file_path, f"{mode}_data.npy"))
+        y = np.load(os.path.join(file_path, f"{mode}_label.npy")).reshape(-1)
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f"Expected file not found: {e.filename}") from e
+
     return X, y
